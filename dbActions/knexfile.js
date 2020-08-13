c2d664f5612c65737b89343dc9ceff5ffb7df5e6
Jonathan Trachtenberg <j.trachtenberg@gmail.com>
2020-08-13 08:04:53 -0700

    fix typo

--- dbActions/knexfile.js
+++ dbActions/knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
       database: 'tmw',
       debug: false,
       typeCast: function (field, next) {
-        if ( (field.type === 'JSON' || field.tyle === 'BLOB') && (field.name === 'poi' || field.name === 'exits' || field.name === 'stateData' || field.name === 'objects')) {
+        if ( (field.type === 'JSON' || field.type === 'BLOB') && (field.name === 'poi' || field.name === 'exits' || field.name === 'stateData' || field.name === 'objects')) {
           return (JSON.parse(field.string()))
         }
         return next()
