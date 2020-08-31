ac6c7af0547ba593e9d9bf2da2051ecceb862a2b
Jonathan Trachtenberg <j.trachtenberg@gmail.com>
2020-08-30 21:43:01 -0700

    remove debug

--- dbActions/auth.js
+++ dbActions/auth.js
@@ -2,19 +2,14 @@ const jwt = require('jsonwebtoken');
 const knex = require('knex')(require('./knexfile'))
 
 module.exports = async (req, res, next) => {
-    console.log('auth')
   try {
     console.log(req.headers)
     const token = req.headers.authorization.split(' ')[1];
     const userId = req.body.userId
     let checkUserId
-    console.log(userId)
     await knex('users').where({userId: userId}).first('salt').then(user => {
         const decodedToken = jwt.verify(token, user.salt);
         checkUserId = decodedToken.userId;
-        console.log(decodedToken)
-
-        console.log(checkUserId)
         if (userId === checkUserId) {
             next();  
         } else {
@@ -22,7 +17,6 @@ module.exports = async (req, res, next) => {
         }
     })
   } catch {
-      console.log('error')
     res.status(401).json({
       error: new Error('Invalid request!')
     });
